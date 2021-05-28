98687e27fb6ef5bc41ed7c38f6713fa1a68bb0c0
wfajczyk <37833669+wfajczyk@users.noreply.github.com>
2021-05-28 11:29:53 +0200

    test: Add property group export test (#30)

--- tests/Functional/ExporterShopware6TestCase.php
+++ tests/Functional/ExporterShopware6TestCase.php
@@ -14,9 +14,6 @@ use Ergonode\ExporterShopware6\Tests\Functional\Fixtures\Kernel;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpKernel\KernelInterface;
 
-/**
- * @method KernelBrowser createClient()
- */
 class ExporterShopware6TestCase extends WebTestCase
 {
     protected static Connection $connection;
@@ -40,5 +37,14 @@ class ExporterShopware6TestCase extends WebTestCase
     protected static function createKernel(array $options = []): Kernel
     {
         return new Kernel('test', false);
+    }
+
+    protected static function createClient(array $options = [], array $server = []): KernelBrowser
+    {
+        /** @var KernelBrowser $client */
+        $client = parent::createClient($options, $server);
+        $client->disableReboot();
+
+        return $client;
     }
 }
